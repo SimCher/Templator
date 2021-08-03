5a6bbd094e0410814cb838e3427a2b73d4af6bb5
Dmitry <dg743733@gmail.com>
2021-08-03 15:23:04 +0700

    Первый коммит

---
+++ WpfApp1/ImageService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Вспомогательный класс для работы с изображениями
+    /// </summary>
+    static class ImageService
+    {
+        /// <summary>
+        /// Вызывает OpenFileDialog и возвращает данные выбранного файла
+        /// </summary>
+        /// <returns></returns>
+        private static Microsoft.Win32.OpenFileDialog ShowOpenFileDialog()
+        {
+            return new()
+            {
+                Filter = "Графические файлы (*.jpg; *.jpeg; *.gif; *.bmp; *.png)|*.jpg; *.jpeg; *.gif; *.bmp; *.png"
+            };
+        }
+
+        /// <summary>
+        /// Открывает диалоговое окно для выбора изображения и возвращает изображение, выбранное пользователем
+        /// </summary>
+        /// <returns>Изображение, выбранное в диалоговом окне</returns>
+        public static Image GetPictureWithOpenFileDialog()
+        {
+            Image image = null;
+            var dialog = ShowOpenFileDialog();
+
+            if ((bool)dialog.ShowDialog())
+            {
+                var bitmap = new BitmapImage(new Uri(dialog.FileName));
+                image = new Image { Source = bitmap };
+            }
+
+            return image;
+        }
+
+        /// <summary>
+        /// Открывает диалоговое окно для выбора фонового изображения и возвращает объект типа ImageBrush
+        /// </summary>
+        /// <returns>Изображение как объект типа ImageBrush</returns>
+        public static ImageBrush GetBackgroundWithOpenFileDialog()
+        {
+            var dialog = ShowOpenFileDialog();
+            ImageBrush imageBrush = null;
+
+            if ((bool) dialog.ShowDialog())
+            {
+                var bitmap = new BitmapImage(new Uri(dialog.FileName));
+                imageBrush = new ImageBrush {ImageSource = bitmap};
+            }
+
+            return imageBrush;
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Image _logo;
+        private Image _draggedImage;
+        private Point _mousePosition;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.Source is Image image && CanvasSpace.CaptureMouse())
+            {
+                _mousePosition = e.GetPosition(CanvasSpace);
+                _draggedImage = image;
+                Panel.SetZIndex(_draggedImage, 1);
+            }
+        }
+
+        private void Canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if(_draggedImage != null)
+            {
+                CanvasSpace.ReleaseMouseCapture();
+                Panel.SetZIndex(_draggedImage, 0);
+                _draggedImage = null;
+            }
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(_draggedImage != null)
+            {
+                var position = e.GetPosition(CanvasSpace);
+                var offset = position - _mousePosition;
+                _mousePosition = position;
+                Canvas.SetLeft(_draggedImage, Canvas.GetLeft(_draggedImage) + offset.X);
+                Canvas.SetTop(_draggedImage, Canvas.GetTop(_draggedImage) + offset.Y);
+            }
+        }
+
+        private void AddLogoButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_logo != null)
+            {
+                if (CanvasSpace.Children.Contains(_logo))
+                {
+                    CanvasSpace.Children.Remove(_logo);
+                }
+            }
+            
+            _logo = ImageService.GetPictureWithOpenFileDialog();
+            Canvas.SetLeft(_logo, 0);
+            Canvas.SetTop(_logo, 0);
+            CanvasSpace.Children.Add(_logo);
+        }
+
+        private void SetBackgroundButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Background = ImageService.GetBackgroundWithOpenFileDialog();
+        }
+    }
+}
